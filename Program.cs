b263237ada30b244a4f39b004f5eb7ff04a2c159
AndersonSantosCarioca <cariocacapoeira3497@gmail.com>
2021-05-20 18:59:01 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace testeVariavel
+{
+    internal class NewBaseType
+    {
+        static void Main(string[] argumentos)  
+        {
+
+            const string constante = "Uma constante que não muda";
+
+            if(argumentos.Length == 0)
+            {
+                Console.WriteLine("favor informar um número inteiro");
+                return;
+            }
+
+            int inteiro;
+            bool teste = int.TryParse(argumentos[0], out inteiro);
+
+            if(teste == false)
+            {
+                Console.WriteLine("O argumento informado não é um número inteiro");
+                return;
+            }
+
+            Console.WriteLine(constante);
+            Console.WriteLine("O número informado foi: " + inteiro);
+
+        }             
+
+        
+    
+    }
+}
